b0bcb790d71841c50a8234f2712943c17c000cbb
Sina <sinafateh1382@gmail.com>
2026-08-17 22:47:12 +0330

    add daphne to top of INSTALLED_APPS

--- chatapp_project/settings.py
+++ chatapp_project/settings.py
@@ -60,6 +60,7 @@ CSRF_TRUSTED_ORIGINS = env_list(
 # ---------------------------------------------------------
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
