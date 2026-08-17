3607c43d36a1faa8fc19f18dceb3d181737e6005
Sina <sinafateh1382@gmail.com>
2026-08-17 22:23:10 +0330

    Update main urls

--- chatapp_project/urls.py
+++ chatapp_project/urls.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
+from django.urls import include, path
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('chat.urls')),
-]
+    path(
+        "admin/",
+        admin.site.urls,
+    ),
 
-# سرویس فایل‌های استاتیک در حالت DEBUG
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    
-    # همچنین برای فایل‌های استاتیک اپلیکیشن‌ها
-    urlpatterns += static('/static/', document_root=settings.STATIC_ROOT)
+    path(
+        "",
+        include("chat.urls"),
+    ),
+]
