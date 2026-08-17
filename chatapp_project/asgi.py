9fecfbd0815403e98c004bf501d9f1887e2f9d86
Sina <sinafateh1382@gmail.com>
2026-08-17 22:22:40 +0330

    Edit asgi.py

--- chatapp_project/asgi.py
+++ chatapp_project/asgi.py
@@ -1,22 +1,34 @@
 import os
+
 from django.core.asgi import get_asgi_application
 
-# ابتدا تنظیمات جنگو را load کنید
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp_project.settings')
 
-# برنامه Django را قبل از هر چیزی بسازید
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "chatapp_project.settings",
+)
+
+
 django_asgi_app = get_asgi_application()
 
-# حالا ایمپورت‌هایی که به مدل‌های جنگو نیاز دارند
-from channels.routing import ProtocolTypeRouter, URLRouter
+
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
 from chat.routing import websocket_urlpatterns
 
-application = ProtocolTypeRouter({
-    'http': django_asgi_app,
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    websocket_urlpatterns
+                )
+            )
+        ),
+    }
+)
